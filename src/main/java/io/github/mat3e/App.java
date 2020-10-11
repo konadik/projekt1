e7e6b34cf1d96689ca029816c99229e7c32caab5
konradik <karinakonradi1@gmail.com>
2020-10-11 13:47:49 +0200

    init commit

---
+++ src/main/java/io/github/mat3e/App.java
@@ -0,0 +1,12 @@
+package io.github.mat3e;
+import org.slf4j.LoggerFactory;
+import org.slf4j.Logger;
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        Logger logger = LoggerFactory.getLogger(App.class);
+        logger.info("Hello World");
+    }
+}
---
+++ src/main/java/io/github/mat3e/HelloServlet.java
@@ -0,0 +1,24 @@
+package io.github.mat3e;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import javax.servlet.annotation.WebServlet;
+
+
+@WebServlet(name ="hello", urlPatterns={"/api/*"})
+
+public class HelloServlet extends HttpServlet {
+    private final Logger logger=LoggerFactory.getLogger(HelloServlet.class);
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doGet(req, resp);
+        logger.info("request got");
+        resp.getWriter().write("hello world");
+    }
+}
